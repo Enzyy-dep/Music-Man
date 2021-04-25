bd3dcc503974ab852ad008079af090b5b2e06d69
Enzyy-dep <enzyyaldjufri@gmail.com>
2021-04-26 03:34:25 +0700

    Kuwuntul

--- handlers/start.py
+++ handlers/start.py
@@ -31,7 +31,7 @@ async def start(_, message: Message):
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
-                    InlineKeyboardButhttps://telegra.ph/BOT-Music-Assistant-Enzyy--Nana-04-17")
+                    InlineKeyboardButton(https://telegra.ph/BOT-Music-Assistant-Enzyy--Nana-04-17")
                   ],[
                     InlineKeyboardButton(
                         "Group Support", url="https://t.me/enzybotmusik"
